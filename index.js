f3abe6997005adf89a8bb5b468b062a6bebb8c87
mfdcoder <48575544+mfdcoder@users.noreply.github.com>
2022-06-17 13:17:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ var server = express();
 
 // This will be call by APPLE TO VERIFY THE APP-SITE-ASSOCIATION 
 // Make the 'apple-app-site-association' accessable to apple to verify the association
-server.get('/.well-known/apple-app-site-association', function(request, response) {
-  response.sendFile(__dirname +  '/apple-app-site-association');
+server.get('/', function(request, response) {
+  response.sendFile(__dirname +  '/index.html');
 });
 
 // HOME PAGE
 server.get('/home', function(request, response) {
-  response.sendFile(__dirname +  '/home.html');
+  response.sendFile(__dirname +  '/index.html');
 });
 
 // ABOUT PAGE
 server.get('/about', function(request, response) {
   response.sendFile(__dirname +  '/about.html');
+});
+
+server.get('/.well-known/apple-app-site-association', function(request, response) {
+  response.sendFile(__dirname +  '/apple-app-site-association');
 });
 
 
